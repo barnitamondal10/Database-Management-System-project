f91938297c1a0f3673f959360eb318db6f2e2334
barnitamondal10 <40147487+barnitamondal10@users.noreply.github.com>
2018-06-11 13:41:18 +0530

    Add files via upload

---
+++ edit_cosmetic_products_php.php
@@ -0,0 +1,117 @@
+<html>
+<head>
+<style type="text/css">
+h1{
+font-size: 50px;
+   color: red;
+}
+h2 {
+font-size: 25px;
+   color: yellow; 
+}
+h3 {
+font-size: 22px;
+   color: white; 
+}
+body{
+background-image: url(edit_cp.jpg);
+background-size: 100%,100%;
+}
+
+.aa{
+width: 1400px;
+height: 800px;
+background-color: rgba(0,0,0,0.4);
+margin:0 auto;
+margin-top: 30px;
+padding-top: 15px;
+padding-left: 50px;
+border-radius: 20px;
+font-weight: bolder;
+font-size: 18px;
+box-shadow: inset 0 50px rgba(255,255,255,0.2),
+	    inset 0 -15px 30px rgba(0,0,0,0.4),
+		  0 5px 10px rgba(0,0,0,0.5);
+}
+.aa input[type="text"]{
+width: 200px;
+height: 30px;
+border: 0;
+border-radius: 5px;
+padding-left: 15px;
+}
+.aa input[type="password"]{
+width: 200px;
+height: 30px;
+border: 0;
+border-radius: 5px;
+padding-left: 15px;
+}
+.aa input[type="submit"]{
+width: 130px;
+height: 40px;
+border: 0;
+border-radius: 5px;
+background-color: yellow;
+font-weight: bolder;
+font-size: 17px;
+}
+
+</style>
+</head>
+<body>
+<div class="aa">
+<head>
+<title>UPDATE COSMETIC_PRODUCT RECORDS</title>
+</head>
+<?php
+$con=mysqli_connect("localhost:3306","root","");
+mysqli_select_db($con,"cosmetics_store");
+
+$sql="SELECT * from cosmetic_product";
+$records=mysqli_query($con,$sql);
+
+
+echo "<h1 align=\"left\"><u><b>COSMETIC PRODUCT DETAILS</h1></b></u>";
+echo "<table width=\"1000\" border=\"1\" align=\"center\" cellpadding=\"1\" cellspacing=\"1\" style=\"line-height:30px:\">";
+ echo  "<tr>";
+echo  "<th><h2>COSMETIC_PRODUCT_ID</h2></th>";
+echo     "<th><h2>COSMETIC_PRODUCT_NAME</h2></th>";  
+echo      "<th><h2>BRAND</h2></th>";
+echo	"<th><h2>PRICE</h2></th>";
+echo	"<th><h2>DESCRIPTION</h2></th>";
+//echo	"<th><h2>STOCK</h2></th>";
+//echo	"<th><h2>DEALER_ID</h2></th>";
+echo   "</tr>";
+
+while($row=mysqli_fetch_assoc($records))
+{
+echo "<tr>";
+echo "<td><h3>".$row['cpid']."</h3></td>";
+echo "<td><h3>".$row['cpname']."</h3></td>";
+echo "<td><h3>".$row['brand']."</h3></td>";
+echo "<td><h3>".$row['price']."</h3></td>";
+echo "<td><h3>".$row['description']."</h3></td>";
+//echo "<td><h3>".$row['stock']."</h3></td>";
+//echo "<td><h3>".$row['did']."</h3></td>";
+?>
+
+<td><a href="update_cosmetic_products_php.php?e_cpid=<?php echo $row['cpid']; ?>" alt="EDIT"><b><h3>EDIT</h3></b></a></td>
+
+<?php
+echo "</tr>";
+}
+echo "</table>";
+
+
+echo "<form action=\"http://localhost/cosmetics/cosmetic_product_options.html\" method=POST>";
+echo "</br></br></br>";
+echo "<input type=\"submit\" name=\"buttond\"  value=\"BACK\">";
+echo"</form>";
+?>
+</body>
+</div>
+</html>
+
+
+
---
+++ edit_order_php.php
@@ -0,0 +1,123 @@
+<html>
+<head>
+<style type="text/css">
+h1{
+font-size: 50px;
+   color: red;
+}
+h2 {
+font-size: 25px;
+   color: yellow; 
+}
+h3 {
+font-size: 22px;
+   color: white; 
+}
+body{
+background-image: url(add_cust.jpg);
+background-size: 100%,100%;
+}
+
+.aa{
+width: 1500px;
+height: 800px;
+background-color: rgba(0,0,0,0.6);
+margin:0 auto;
+margin-top: 30px;
+padding-top: 15px;
+padding-left: 50px;
+border-radius: 20px;
+font-weight: bolder;
+font-size: 18px;
+box-shadow: inset 0 50px rgba(255,255,255,0.2),
+	    inset 0 -15px 30px rgba(0,0,0,0.4),
+		  0 5px 10px rgba(0,0,0,0.5);
+}
+.aa input[type="text"]{
+width: 200px;
+height: 30px;
+border: 0;
+border-radius: 5px;
+padding-left: 15px;
+}
+.aa input[type="password"]{
+width: 200px;
+height: 30px;
+border: 0;
+border-radius: 5px;
+padding-left: 15px;
+}
+.aa input[type="submit"]{
+width: 130px;
+height: 40px;
+border: 0;
+border-radius: 5px;
+background-color: yellow;
+font-weight: bolder;
+font-size: 17px;
+}
+
+</style>
+</head>
+<body>
+<div class="aa">
+<head>
+<title>UPDATE ORDER RECORDS</title>
+</head>
+<?php
+$con=mysqli_connect("localhost:3306","root","");
+mysqli_select_db($con,"cosmetics_store");
+
+$sql="SELECT * from orders";
+$records=mysqli_query($con,$sql);
+
+echo "<h1 align=\"left\"><u><b>ORDER DETAILS</h1></b></u>";
+echo "<table width=\"1000\" border=\"1\" align=\"center\" cellpadding=\"1\" cellspacing=\"1\" style=\"line-height:30px:\">";
+ echo  "<tr>";
+
+echo	"<th><h2>ORDER_ID</h2></th>";
+echo     "<th><h2>ORDER_DATE</h2></th>";  
+echo      "<th><h2>QUANTITY</h2></th>";
+echo	"<th><h2>TOTAL_PRICE</h2></th>";
+echo	"<th><h2>PAYMENT_TYPE</h2></th>";
+echo	"<th><h2>CUSTOMER_ID</h2></th>";
+echo	"<th><h2>COSMETIC_PRODUCT_ID</h2></th>";
+echo   "</tr>";
+
+
+while($row=mysqli_fetch_assoc($records))
+{
+echo "<tr>";
+echo "<td><h3>".$row['or_id']."</h3></td>";
+echo "<td><h3>".$row['or_date']."</h3></td>";
+echo "<td><h3>".$row['quantity']."</h3></td>";
+echo "<td><h3>".$row['total_price']."</h3></td>";
+echo "<td><h3>".$row['ptype']."</h3></td>";
+echo "<td><h3>".$row['cust_id']."</h3></td>";
+echo "<td><h3>".$row['cpid']."</h3></td>";
+?>
+
+<td><a href="update_order_php.php?e_or_id=<?php echo $row['or_id']; ?>" alt="EDIT"><b><h3>EDIT</h3></b></a></td>
+
+<?php
+echo "</tr>";
+}
+echo "</table>";
+
+
+echo "<form action=\"http://localhost/cosmetics/order_options.html\" method=POST>";
+echo "</br></br></br>";
+echo "<input type=\"submit\" name=\"buttond\"  value=\"BACK\">";
+echo"</form>";
+?>
+</body>
+</div>
+</html>
+
+
+
+
+
+
+
+
